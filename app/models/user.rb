826f765f363a88dac8adde3b8525c49ffe0dbbc6
shogo.yamada <superman199323@gmail.com>
2017-09-10 15:24:41 +0900

    feature add Twitter login and save user info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
 
-  def create_user(auth)
-    user.provider = auth['provider']
-    user.uid = auth['uid']
-    user.screen_name = auth['info']['nickname']
-    user.name = auth['info']['name']
-    user.token = auth.credentials.token
-    user.secret = auth.credentials.secret
+  def self.create_user(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      user.sreen_name = auth['info']['nickname']
+      user.name = auth['info']['name']
+      user.token = auth.credentials.token
+      user.secret = auth.credentials.secret
+    end
   end
-  
+
 end
