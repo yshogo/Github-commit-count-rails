2a5082f284c93bb6e6c617bdc24927e9d2342065
shogo.yamada <superman199323@gmail.com>
2017-09-10 20:22:59 +0900

    user info select & user twitter post

--- app/controllers/tweet_api_controller.rb
+++ app/controllers/tweet_api_controller.rb
@@ -3,18 +3,22 @@ class TweetApiController < ApplicationController
   def tweet
     require_relative '../services/parse_json_service.rb'
 
-    init
-    commit_count = ParseJsonService.new.create_tweet
-    update(create_text(commit_count))
+    user_list = User.all
+
+    user_list.each{ |user|
+      init(user.token, user.secret)
+      commit_count = ParseJsonService.new.create_tweet
+      update(create_text(commit_count, user.GithubId))
+    }
   end
 
   private
-  def init
+  def init(user_token,user_secret)
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key = "nRlCKe4Aug0qyu7CIdmYBmIxc"
       config.consumer_secret = "B02QAH13KqLC68G2hdVdPmyLbNMlfRCCOM7KFDXWK0ycdWRpO6"
-      config.access_token = "772029775674155008-ULcfH540dwvTbDZtvvUvu4MmbkNt9fX"
-      config.access_token_secret = "xXKEfHZMGmp5z0RKBUlEsoYLrFlIxfCStfwNHnwkFtZ2z"
+      config.access_token = user_token
+      config.access_token_secret = user_secret
     end
   end
 
@@ -29,7 +33,7 @@ class TweetApiController < ApplicationController
   end
 
   private
-  def create_text(count)
-    '今日はGithubに' + count.to_s + '回commitしました。https://github.com/yshogo'
+  def create_text(count, github_id)
+    '今日はGithubに' + count.to_s + '回commitしました。https://github.com/' + github_id
   end
 end
