fa5e909f2e0104995b853cecbc027160e517c7a6
shogo.yamada <superman199323@gmail.com>
2017-09-10 14:46:18 +0900

    add controller base home settions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+  def create_user(auth)
+    user.provider = auth['provider']
+    user.uid = auth['uid']
+    user.screen_name = auth['info']['nickname']
+    user.name = auth['info']['name']
+    user.token = auth.credentials.token
+    user.secret = auth.credentials.secret
+  end
+  
 end
